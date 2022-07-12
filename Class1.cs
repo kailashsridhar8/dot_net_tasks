4e7da0c913c1e3410f726ad18516c3dac822c55c
kailashsridhar8 <58856704+kailashsridhar8@users.noreply.github.com>
2022-07-12 17:05:57 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public void DivisionMethod()
+        {
+            double x;
+            double y;
+            double result=0;
+            try
+            {
+                Console.Write("Enter first number : ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number : ");
+            y = Convert.ToInt32(Console.ReadLine());
+           
+                result = x / y;
+                Console.WriteLine("Output: " + result);
+            }
+
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine("You cannot divide by zero");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter numbers only");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong");
+            }
+            finally
+            {
+                Console.WriteLine("End of the code");
+            }
+        }
+
+        public void arrayOutOfBounds()
+        {
+            string[] names = { "Hari", "Eswara", "Kailash", "Avinash", "Iniyan" };
+            for (int i = 0; i <= names.Length; i++)
+            {
+                string SomeName = names[i];
+            }
+        }
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var demo = new Class1();
+            try
+            {
+                demo.DivisionMethod();
+                demo.arrayOutOfBounds();
+            }
+         
+            catch (Exception ex)
+            {
+                string err = ex.Message.ToString();
+                Console.Error.WriteLine(err);
+            }
+        }
+    }
+}
